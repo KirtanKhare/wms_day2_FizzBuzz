3f935fbec93918092fd7bd81290b11b96e850154
[EXT] Kirtan Khare <conkkha@partner.waters.com>
2026-07-24 16:08:14 +0530

    Add console app with FizzBuzz and gitignore

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Name: Kirtan Khare");
+        Console.WriteLine($"Date: {DateTime.Today:24-07-2026}");
+        Console.WriteLine();
+
+        FizzBuzz();
+    }
+
+    static void FizzBuzz()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                Console.WriteLine("FizzBuzz");
+            else if (i % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (i % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(i);
+        }
+    }
+}
